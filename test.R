1bfdf59a5c74f0cb64b5ce62ac472dbdc955d892
djs laptop <dschaffersmith@gmail.com>
2019-11-05 16:51:46 -0500

    Effect of R global options
    Testing format changes imposed by RStudio Global Options.

--- test.R
+++ test.R
@@ -9,3 +9,5 @@ z = x + y
 print(x)
 
 #here is a very very long line for dannica to test and see whether really long lines will make problems when we commit thingys
+
+# Adding a line to see if this affects the formatting on someone else's files because that would be bad.
